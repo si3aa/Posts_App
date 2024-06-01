7b006af9328f8e54ab31ae974f1d28a4e7aa5284
si3aa <mahmoud22said22@gmail.com>
2024-06-01 14:46:24 +0300

    complete(post_local_data,post_remote_data)and finish dataLayer

--- lib/features/posts/data/data_sourses/post_local_data_source.dart
+++ lib/features/posts/data/data_sourses/post_local_data_source.dart
@@ -1,21 +1,40 @@
-import 'package:dartz/dartz.dart';
-import 'package:posts/features/posts/data/models/post_model.dart';
+import 'dart:convert';
 
-abstract class PostLocalDataSource{
+import 'package:dartz/dartz.dart';
+import 'package:posts/core/error/exception.dart';
+import 'package:posts/features/posts/data/models/post_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class PostLocalDataSource {
   Future<List<PostModel>> getCachedPosts();
-  Future<Unit> cachePosts(List<PostModel> posts);
+  Future<Unit> cachePosts(List<PostModel> postModels);
 }
-class PostLocalDataSourceImpl implements PostLocalDataSource{
+const CACHED_POSTS ="CACHED_POSTS";
+
+class PostLocalDataSourceImpl implements PostLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  PostLocalDataSourceImpl({required this.sharedPreferences});
   @override
-  Future<Unit> cachePosts(List<PostModel> posts) {
-    // TODO: implement cachePosts
-    throw UnimplementedError();
+  Future<Unit> cachePosts(List<PostModel> postModels) {
+    List postModelToJson = postModels
+        .map<Map<String, dynamic>>((postModel) => postModel.toJson())
+        .toList();
+    sharedPreferences.setString(CACHED_POSTS, json.encode(postModelToJson));
+    return Future.value(unit);
   }
 
   @override
   Future<List<PostModel>> getCachedPosts() {
-    // TODO: implement getCachedPosts
-    throw UnimplementedError();
+    final jsonString = sharedPreferences.getString(CACHED_POSTS);
+    if (jsonString != null) {
+      List decodeJsonData = json.decode(jsonString);
+      List<PostModel> jsonToPostModels = decodeJsonData
+          .map<PostModel>((jsonPostModel) => PostModel.fromJson(jsonPostModel))
+          .toList();
+      return Future.value(jsonToPostModels);
+    } else {
+      throw EmptyCacheFailureException;
+    }
   }
-
 }
--- lib/features/posts/data/data_sourses/post_remote_data_source.dart
+++ lib/features/posts/data/data_sourses/post_remote_data_source.dart
@@ -1,35 +1,88 @@
+import 'dart:convert';
+
 import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' as http;
+import 'package:posts/core/error/exception.dart';
 import 'package:posts/features/posts/data/models/post_model.dart';
 
-abstract class PostRemoteDataSource{
+abstract class PostRemoteDataSource {
   Future<List<PostModel>> getAllPosts();
-  Future<Unit>deletePosts(int postId);
-  Future<Unit>updatePosts(PostModel postModel);
-  Future<Unit>addPosts(PostModel postModel);
+  Future<Unit> deletePosts(int postId);
+  Future<Unit> updatePosts(PostModel postModel);
+  Future<Unit> addPosts(PostModel postModel);
 }
-class PostRemoteDataSourceImpl implements PostRemoteDataSource{
+
+// ignore: constant_identifier_names
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+class PostRemoteDataSourceImpl implements PostRemoteDataSource {
+  final http.Client client;
+
+  PostRemoteDataSourceImpl({required this.client});
   @override
-  Future<Unit> addPosts(PostModel postModel) {
-    // TODO: implement addPosts
-    throw UnimplementedError();
+  Future<List<PostModel>> getAllPosts() async {
+    final response = await client.get(
+      Uri.parse("$BASE_URL/posts/"),
+      headers: {"Content-Type": "application/json"},
+    );
+    if (response.statusCode == 200) {
+      final List decodedJson = json.decode(response.body) as List;
+      final List<PostModel> postModels = decodedJson
+          .map<PostModel>((jsonPostModel) => PostModel.fromJson(jsonPostModel))
+          .toList();
+      return postModels;
+    } else {
+      throw ServerFailureException();
+    }
   }
 
   @override
-  Future<Unit> deletePosts(int postId) {
-    // TODO: implement deletePosts
-    throw UnimplementedError();
+  Future<Unit> addPosts(PostModel postModel)async {
+   final body ={
+     "title": postModel.title,
+     "body": postModel.body
+   };
+   final response = await client.post(
+      Uri.parse("$BASE_URL/posts/"),
+      body: body);
+      if(response.statusCode==201){
+        return Future.value(unit);
+      }else{
+        throw ServerFailureException();
+      }
   }
 
   @override
-  Future<List<PostModel>> getAllPosts() {
-    // TODO: implement getAllPosts
-    throw UnimplementedError();
+  Future<Unit> deletePosts(int postId)async {
+    final response = await client.delete(
+      Uri.parse("$BASE_URL/posts/${postId.toString()}"),
+      headers: {"Content-Type": "application/json"},
+    );
+    if(response.statusCode==200){
+      return Future.value(unit);
+    }else{
+      throw ServerFailureException();
+    }
+    
   }
 
   @override
-  Future<Unit> updatePosts(PostModel postModel) {
-    // TODO: implement updatePosts
-    throw UnimplementedError();
-  }
+  Future<Unit> updatePosts(PostModel postModel)async {
+    final postId=postModel.id.toString();
+    final body ={
+      "title": postModel.title,
+      "body": postModel.body
+    };
+    final response = await client.patch(
+      Uri.parse("$BASE_URL/posts/$postId"),
+      body: body,
+    );
+    if(response.statusCode==200){
+      return Future.value(unit);
+    }else{
+      throw ServerFailureException();
+    }
 
+   
+  }
 }
