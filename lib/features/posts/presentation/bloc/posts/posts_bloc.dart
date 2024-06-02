e7cdec810f424ef01f6520dae02194834aad7db1
si3aa <mahmoud22said22@gmail.com>
2024-06-02 14:52:27 +0300

    create bloc and handle state in presntationLayer

---
+++ lib/features/posts/presentation/bloc/posts/posts_bloc.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:posts/core/error/failure.dart';
+import 'package:posts/core/messages/failure_message.dart';
+
+import 'package:posts/features/posts/domain/use_cases/get_all_posts.dart';
+
+import '../../../domain/entities/posts.dart';
+
+part 'posts_event.dart';
+part 'posts_state.dart';
+
+class PostsBloc extends Bloc<PostsEvent, PostsState> {
+  final GetAllPostsUseCase getAllPosts;
+  PostsBloc({
+    required this.getAllPosts,
+  }) : super(PostsInitial()) {
+    on<PostsEvent>((event, emit) async {
+      if (event is GetAllPostsEvent) {
+        emit(LoadingPostsState());
+        final failureOrPosts = await getAllPosts();
+        emit(mapFailureOrPostsToState(failureOrPosts));
+      } else if (event is RefreshPostsEvent) {
+        emit(LoadingPostsState());
+        final failureOrPosts = await getAllPosts();
+        emit(mapFailureOrPostsToState(failureOrPosts));
+      }
+    });
+  }
+  PostsState mapFailureOrPostsToState(Either<Failure, List<Post>> either) {
+    return either.fold(
+        (failure) => ErrorPostsState(
+              message: mapFailureTOMessage(failure),
+            ),
+        (posts) => LoadedPostsState(posts: posts));
+  }
+
+  String mapFailureTOMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure _:
+        return serverFailureMessage;
+      case EmptyCacheFailure _:
+        return emptyCacheFailureMessage;
+      case OfflineFailure _:
+        return offlineFailureMessage;
+      default:
+        return "Unexpected Error , Please try again later .";
+    }
+  }
+}
