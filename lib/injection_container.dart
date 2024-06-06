30955b1050c9477a02e860d4f29b83223f6a020c
si3aa <mahmoud22said22@gmail.com>
2024-06-06 15:34:48 +0300

    dependency_injection

---
+++ lib/injection_container.dart
@@ -0,0 +1,50 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:posts/core/network/network_info.dart';
+import 'package:posts/features/posts/data/data_sourses/post_local_data_source.dart';
+import 'package:posts/features/posts/data/data_sourses/post_remote_data_source.dart';
+import 'package:posts/features/posts/data/repository/posts_repo_ipm.dart';
+import 'package:posts/features/posts/domain/repo/posts_repo.dart';
+import 'package:posts/features/posts/domain/use_cases/add_posts.dart';
+import 'package:posts/features/posts/domain/use_cases/delete_posts.dart';
+import 'package:posts/features/posts/domain/use_cases/get_all_posts.dart';
+import 'package:posts/features/posts/domain/use_cases/updata_posts.dart';
+import 'package:posts/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
+import 'package:posts/features/posts/presentation/bloc/posts/posts_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+//Bloc
+  sl.registerFactory(() => PostsBloc(getAllPosts: sl()));
+  sl.registerFactory(() => AddDeleteUpdatePostBloc(
+      addPost: sl(), deletePost: sl(), updatePost: sl()));
+
+  // UseCases
+  sl.registerLazySingleton(() => GetAllPostsUseCase(sl()));
+  sl.registerLazySingleton(() => AddPostsUseCase(sl()));
+  sl.registerLazySingleton(() => DeletePostsUseCase(sl()));
+  sl.registerLazySingleton(() => UpdatePostsUseCase(sl()));
+
+  // Repository
+  sl.registerLazySingleton<PostsRepository>(() => PostsRepositoryIpm(
+      localDataSource: sl(), remoteDataSource: sl(), networkInfo: sl()));
+
+  // DataSources
+  sl.registerLazySingleton<PostRemoteDataSource>(
+      () => PostRemoteDataSourceImpl(client: sl()));
+  sl.registerLazySingleton<PostLocalDataSource>(
+      () => PostLocalDataSourceImpl(sharedPreferences: sl()));
+
+  // core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  //external
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+
+  sl.registerLazySingleton(() => http.Client());
+   sl.registerLazySingleton(() => InternetConnectionChecker());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:posts/core/theme.dart';
+import 'package:posts/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
+import 'package:posts/features/posts/presentation/bloc/posts/posts_bloc.dart';
+import 'injection_container.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const MyApp());
 }
 
@@ -10,20 +16,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Posts App',
-      theme: appTheme,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Posts',
-            style: TextStyle(
-                color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => di.sl<PostsBloc>()),
+        BlocProvider(create: (_) => di.sl<AddDeleteUpdatePostBloc>()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Posts App',
+        theme: appTheme,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Posts',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
-        body: const Center(
-          child: Text('Posts App'),
+          body: const Center(
+            child: Text('Posts App'),
+          ),
         ),
       ),
     );
